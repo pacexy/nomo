9ac923152619454efb98f3b154e69cd1b35ecd32
Stephen Zhou <hi@hyoban.cc>
2023-07-29 21:23:17 +0800

    feat: add Japanese support (#1)
    * feat: add Japanese support
    
    * fix: add dict files
    
    * fix: path
    
    * fix: path
    
    * fix: kuroshiro has already been initialized
    
    ---------
    
    Co-authored-by: pacexy <pacexy@gmail.com>

--- app/routes/translate.tsx
+++ app/routes/translate.tsx
@@ -1,8 +1,14 @@
 import { GoogleTranslator } from '@translate-tools/core/translators/GoogleTranslator'
 import { langCode } from '@translate-tools/core/types/Translator'
+// @ts-ignore
+import Kuroshiro from 'kuroshiro'
+// @ts-ignore
+import KuromojiAnalyzer from 'kuroshiro-analyzer-kuromoji'
 import { LoaderFunction } from 'remix'
 
 const translator = new GoogleTranslator()
+const kuroshiro = new Kuroshiro()
+let initializedKuroshiro = false
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url)
@@ -12,7 +18,26 @@ export const loader: LoaderFunction = async ({ request }) => {
 
   if (!text) return null
   const data = await Promise.all(
-    tos.map((to) => translator.translate(text, 'auto', to as langCode)),
+    tos.map(async (to) => {
+      let result = await translator.translate(text, 'auto', to as langCode)
+      if (to === 'ja') {
+        if (!initializedKuroshiro) {
+          await kuroshiro.init(
+            new KuromojiAnalyzer({
+              dictPath: __dirname + '/../dict',
+            }),
+          )
+          initializedKuroshiro = true
+        }
+
+        result = await kuroshiro.convert(result, {
+          to: 'romaji',
+          mode: 'normal',
+          romajiSystem: 'passport',
+        })
+      }
+      return result
+    }),
   )
 
   return data
